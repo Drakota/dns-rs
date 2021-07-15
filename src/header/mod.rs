961bb19229c6fd3e3c11b0b4e1ecd2b5f0b5837a
Jonathan Bouchard <dev.drakota@gmail.com>
2021-07-14 21:53:23 -0400

    Started DnsHeader serialization with the cookie-factory crate

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -3,8 +3,8 @@ mod utils;
 
 use self::flags::DnsHeaderFlags;
 use crate::types::{ParseInput, ParseResult};
-
-use nom::{bits::bits, combinator::map, error::context, number::complete::be_u16, sequence::tuple};
+use cookie_factory::{self as cf, gen_simple, GenError, SerializeFn};
+use std::io::Write;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct DnsHeader {
@@ -18,12 +18,16 @@ pub struct DnsHeader {
 
 impl DnsHeader {
     pub fn parse(i: ParseInput) -> ParseResult<Self> {
+        use nom::{
+            bits::bits, combinator::map, error::context, number::complete::be_u16, sequence::tuple,
+        };
+
         map(
             tuple((
                 context("Transaction ID", be_u16),
                 context("Flags", bits(DnsHeaderFlags::parse)),
+                context("Queries", be_u16),
                 context("Responses", be_u16),
-                context("Answers", be_u16),
                 context("Authority RRs", be_u16),
                 context("Additional RRs", be_u16),
             )),
@@ -36,5 +40,47 @@ impl DnsHeader {
                 add_rr,
             },
         )(i)
+    }
+
+    pub fn serialize<W: Write>(&self) -> impl SerializeFn<W> {
+        use cf::{bytes::be_u16, sequence::tuple};
+
+        tuple((
+            be_u16(self.transaction_id),
+            be_u16(self.queries),
+            be_u16(self.responses),
+            be_u16(self.auth_rr),
+            be_u16(self.add_rr),
+        ))
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, GenError> {
+        gen_simple(self.serialize(), Vec::new())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialize() {
+        let header = DnsHeader {
+            transaction_id: 0x1234,
+            flags: DnsHeaderFlags::default(),
+            queries: 0x5678,
+            responses: 0x9abc,
+            auth_rr: 0xdef0,
+            add_rr: 0xabcd,
+        };
+
+        #[rustfmt::skip]
+        assert_eq!(header.to_bytes().unwrap(), vec![
+            0x12, 0x34, // Transaction ID
+            0x56, 0x78, // Queries
+            0x9A, 0xBC, // Responses
+            0xDE, 0xF0, // Auth RRs
+            0xAB, 0xCD, // Additional RRs
+        ]);
     }
 }
